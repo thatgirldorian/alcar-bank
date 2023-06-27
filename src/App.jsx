370ae534166235921556f8de6fb694d27a39f08c
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-06-27 10:27:11 +0400

    feat: added some custom styles and constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <h1>Alcar Bank</h1>
+  </div>
+);
 
 export default App;
