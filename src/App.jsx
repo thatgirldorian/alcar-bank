d5032477a88025a5a6ee54c4858f0b66ec2087de
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-06-27 10:51:25 +0400

    feat: added scaffolding for rest of website + components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Alcar Bank</h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>NavBar Section</div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>Hero section</div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats Business Billing CardDeal Testimonials Clients CTA Footer
+      </div>
+    </div>
   </div>
 );
 
