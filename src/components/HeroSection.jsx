fa8dc6c8b87e748c66a2ed5a00cdfc9cc0cf8d25
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-06-30 08:54:34 +0400

    finished hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -46,7 +46,9 @@ const HeroSection = () => {
         </p>
       </div>
 
-      <div>
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
         <img
           src={robot}
           alt="robot-hand-img"
@@ -55,6 +57,10 @@ const HeroSection = () => {
         <div className="absolute z-0 w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[85%] rounded-full bottom-0 white__gradient" />
         <div className="absolute z-0 w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
       </div>
     </section>
   );
