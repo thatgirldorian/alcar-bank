ae5aa9c32f000d15da97a0468b0b81275678daa8
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-07-02 09:56:19 +0400

    added the business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Focus on your business, <br className="sm:block hidden" /> leave the
+          finances to us.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Discover how the right credit card can transform your financial
+          journey by aiding you in building credit, earning enticing rewards,
+          and maximizing savings in the midst of a vast credit card landscape.
+        </p>
+        <Button />
+      </div>
+    </section>
+  );
 };
 
 export default Business;
