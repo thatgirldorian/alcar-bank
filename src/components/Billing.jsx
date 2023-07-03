528f321706b10e6608682167a2c6f2c443711a87
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-07-03 18:23:21 +0400

    wrapped up  billing section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -15,7 +15,31 @@ const Billing = () => {
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
       </div>
 
-      <div className={layout.sectionInfo}></div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Effortlessly manage <br className="sm:hidden" /> your billing &
+          invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          From effortless billing and invoicing to comprehensive financial
+          control, we empower you to take charge of your finances and focus on
+          what matters most – growing your business.
+        </p>
+
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="apple-store-logo"
+            className="w-[160px] h-[32px] object-contain mr-5 cursor-pointer"
+          />
+
+          <img
+            src={google}
+            alt="google-play-logo"
+            className="w-[160px] h-[32px] object-contain mr-5 cursor-pointer"
+          />
+        </div>
+      </div>
     </section>
   );
 };
