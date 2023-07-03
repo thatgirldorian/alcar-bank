d242092a4caf7fc74a1fa24e51a5f5bbd2e0b8d0
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-07-03 18:31:01 +0400

    added card deal component

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" /> in a few
+          easy steps.
+        </h2>
+        <p className={`${styles.paragraph} w-[470px] mt-5`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
