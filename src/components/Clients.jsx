62d9d48bf668d1cda9fb1207771abb44d01405d3
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-07-04 13:19:26 +0400

    added client logos

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+          >
+            <img
+              src={client.logo}
+              alt="company-logo"
+              className="sm:w-[192px] w-[100px] object-contain hover:brightness-150 hover:filter hover:invert"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
