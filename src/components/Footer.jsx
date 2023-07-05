d2bfa7ae0dfc90bed40ef13361d57994723bab7d
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-07-05 18:14:37 +0400

    added social media icon + hr

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,6 +43,24 @@ const Footer = () => {
           ))}
         </div>
       </div>
+
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+        <p className="font-poppins font-normal text-center text-[16px] leading-[27px] text-white">
+          Copyright 2023, AlcarBank. All Rights Reserved.
+        </p>
+        <div className="flex flex-row md:mt-0 mt-6">
+          {socialMedia.map((social, index) => (
+            <img
+              key={social.id}
+              src={social.icon}
+              alt="social-media-icon"
+              className={`w-[21px] h-[21px] cursor-pointer object-contain ${
+                index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+              }`}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
